e2e889a9fc259d012b910bf981c1da7dd4c33b3e
unknown <weblixin@163.com>
2018-07-05 18:52:52 +0800

    订单模块

---
+++ src/service/address-service.js
@@ -0,0 +1,151 @@
+
+'use strict';
+
+var _mm = require('util/mm.js');
+
+var _cart = {
+    // 获取购物车数量
+    getCartCount : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/get_cart_product_count.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 添加到购物车
+    addToCart : f/*
+* @Author: Rosen
+* @Date:   2017-06-06 09:25:41
+* @Last Modified by:   Rosen
+* @Last Modified time: 2017-06-09 19:49:33
+*/
+
+'use strict';
+var _mm = require('util/mm.js');
+
+var _order = {
+    // 获取商品列表
+    getProductList : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/get_order_cart_product.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 提交订单
+    createOrder : function(orderInfo, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/create.do'),
+            data    : orderInfo,
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取订单列表
+    getOrderList : function(listParam, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/list.do'),
+            data    : listParam,
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取订单详情
+    getOrderDetail : function(orderNumber, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/detail.do'),
+            data    : {
+                orderNo : orderNumber
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 取消订单
+    cancelOrder : function(orderNumber, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/cancel.do'),
+            data    : {
+                orderNo : orderNumber
+            },
+            success : resolve,
+            error   : reject
+        });
+    }
+}
+module.exports = _order;unction(productInfo, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/add.do'),
+            data    : productInfo,
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取购物车列表
+    getCartList : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/list.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 选择购物车商品
+    selectProduct : function(productId, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/select.do'),
+            data    : {
+                productId : productId
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 取消选择购物车商品
+    unselectProduct : function(productId, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/un_select.do'),
+            data    : {
+                productId : productId
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 选中全部商品
+    selectAllProduct : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/select_all.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 取消选中全部商品
+    unselectAllProduct : function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/un_select_all.do'),
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 更新购物车商品数量
+    updateProduct : function(productInfo, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/update.do'),
+            data    : productInfo,
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 删除指定商品
+    deleteProduct : function(productIds, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/cart/delete_product.do'),
+            data    : {
+                productIds : productIds
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+}
+module.exports = _cart;
